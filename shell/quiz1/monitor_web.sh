b54a8926fe6122a12d71ecc59483af8ec52321ad
bjdzliu <bjdzliu@users.noreply.github.com>
2017-11-08 07:32:39 +0800

    Update monitor_web.sh

--- shell/quiz1/monitor_web.sh
+++ shell/quiz1/monitor_web.sh
@@ -13,9 +13,9 @@ httpcode=`curl -o /dev/null -s -w %{http_code} "$weburl"`
 echo $httpcode
 if [ $httpcode != 200 ]
 then
-echo "web on linuxone is down!" | mail -s "web on linuxone is down" bjdzliu@cn.ibm.com
-echo "web on linuxone is down!" | mail -s "web on linuxone is down" zhengxil@cn.ibm.com
-echo "web on linuxone is down!" | mail -s "web on linuxone is down" yeqi@cn.ibm.com@cn.ibm.com
+echo "web on linuxone is down!" | mail -s "web on linuxone is down" 1@cn.ibm.com
+echo "web on linuxone is down!" | mail -s "web on linuxone is down" 2@cn.ibm.com
+echo "web on linuxone is down!" | mail -s "web on linuxone is down" 3@cn.ibm.com
 #echo "web is down!" 
 else 
 {
@@ -33,9 +33,9 @@ if [ $? -eq 0 ]
 then
 echo "web-content is checked ok! `date '+%Y-%m-%d %H:%M:%S'`" >> $logfile
 else
-echo "web content have error " | mail -s "web content error" bjdzliu@cn.ibm.com
-echo "web content have error " | mail -s "web content error" zhengxil@cn.ibm.com
-echo "web content have error " | mail -s "web content error" yeqi@cn.ibm.com@cn.ibm.com
+echo "web content have error " | mail -s "web content error" 1@cn.ibm.com
+echo "web content have error " | mail -s "web content error" 2@cn.ibm.com
+echo "web content have error " | mail -s "web content error" 3@cn.ibm.com
 #echo "web content have error "
 fi
 fi
@@ -45,9 +45,9 @@ then
 httptime=`curl -o /dev/null -s -w "time_connect: %{time_connect}\ntime_starttransfer:%{time_starttransfer}\ntime_total: %{time_total}\n" "$weburl"|grep time_total|awk -F ":" '{print $2*1000}'`
 if [ $httptime -ge 60000 ]
 then
-echo "web is timeout!,Response time is longer than 1min" | mail -s "WEB on linuxone Response time is longer than 1min" bjdzliu@cn.ibm.com
-echo "web is timeout!,Response time is longer than 1min" | mail -s "WEB on linuxone Response time is longer than 1min" zhengxil@cn.ibm.com
-echo "web is timeout!,Response time is longer than 1min" | mail -s "WEB on linuxone Response time is longer than 1min" yeqi@cn.ibm.com@cn.ibm.com
+echo "web is timeout!,Response time is longer than 1min" | mail -s "WEB on linuxone Response time is longer than 1min" 1@cn.ibm.com
+echo "web is timeout!,Response time is longer than 1min" | mail -s "WEB on linuxone Response time is longer than 1min" 2@cn.ibm.com
+echo "web is timeout!,Response time is longer than 1min" | mail -s "WEB on linuxone Response time is longer than 1min" 3@cn.ibm.com
 #echo "web is timeout!,Response time is longer than 10sec" 
 else
 echo "web is connect ok! at `date '+%Y-%m-%d %H:%M:%S'` RT is about $httptime " >> /root/monitorLinuxOneSite/monitor.log
